b4e14c6c0e7fe5db32f53c9db5fb5f8e8ab83c74
Sachin Singh <sachinsingh9272@gmail.com>
2025-07-02 00:01:28 +0530

    Database setup

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try{
+    await mongoose.connect(process.env.MONGO_URL)    
+    console.log(`Connected to Database💽`);
+    
+  }catch(error){
+    console.log(`MONGO Connect Error`);
+  }
+}
+
+export default connectDB;
